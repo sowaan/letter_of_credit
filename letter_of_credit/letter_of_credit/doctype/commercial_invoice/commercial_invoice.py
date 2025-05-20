04d773c2dab3d0a1245cda6a3d8d7a3fa798b5bf
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2025-05-20 18:18:31 +0500

    Update for commercial invoice doctype.

--- letter_of_credit/letter_of_credit/doctype/commercial_invoice/commercial_invoice.py
+++ letter_of_credit/letter_of_credit/doctype/commercial_invoice/commercial_invoice.py
@@ -9,6 +9,21 @@ from frappe.utils import money_in_words
 class CommercialInvoice(Document):
 
 	def before_save(self) :
+
+		self.setting_sales_invoice_items()
+		self.calculate_items_total()
+	
+		self.update_tax_table()
+		self.calculate_total_taxes_and_charges()
+
+		self.grand_total = self.total_amount + self.total_taxes_and_charges
+
+		self.calculating_foreign_currency()
+		self.in_words = money_in_words(self.grand_total)
+
+
+
+	def calculate_items_total(self) :
 		total_quantity = 0
 		total_amount = 0
 
@@ -22,14 +37,43 @@ class CommercialInvoice(Document):
 		self.total_amount = total_amount
 		self.amount_in_foreign_currency = self.total_amount * self.exchange_rate
 
-		self.update_tax_table()
-		self.calculate_total_taxes_and_charges()
+	def setting_sales_invoice_items(self) :
+		si_net_total = 0
+		si_vat = 0
+		si_grand_total = 0
+		self.items = []
+		item_map = {}
 
-		self.grand_total = self.total_amount + self.total_taxes_and_charges
-
-		self.calculating_foreign_currency()
-		self.in_words = money_in_words(self.grand_total)
-
+		if self.sales_invoices :
+			for row in self.sales_invoices :
+				
+				si_net_total = si_net_total + row.total
+				si_vat = si_vat + row.vat
+				si_grand_total = si_grand_total + row.grand_total
+	
+				si_doc = frappe.get_doc('Sales Invoice', row.sales_invoice)
+				if si_doc.items:
+					for item in si_doc.items:
+						key = (item.item_code, item.uom, item.rate)
+						if key in item_map:
+							item_map[key]['quantity'] += item.qty
+							item_map[key]['amount'] += item.amount
+						else:
+							item_map[key] = {
+								'item_code': item.item_code,
+								'item_name': item.item_name,
+								'uom': item.uom,
+								'description': item.description,
+								'quantity': item.qty,
+								'rate': item.rate,
+								'amount': item.amount
+							}
+				
+		self.si_net_total = si_net_total
+		self.si_vat = si_vat
+		self.si_grand_total = si_grand_total
+		for item in item_map.values():
+			self.append('items', item)
 
 	def update_tax_table(self) :
 		if self.sales_taxes_and_charges :
@@ -44,10 +88,82 @@ class CommercialInvoice(Document):
 				total_taxes_and_charges = total_taxes_and_charges + row.tax_amount
 		self.total_taxes_and_charges = total_taxes_and_charges
 
-
 	def calculating_foreign_currency(self) :
 		self.total_taxes_and_charges_in_foreign_currency = self.total_taxes_and_charges * self.exchange_rate
 		self.grand_total_in_for_currency = self.grand_total * self.exchange_rate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
